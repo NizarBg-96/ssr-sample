7efbd2634ebafd61ca93031c84ef90a6ac5ea0f2
Nizar <nizar@atayen.us>
2022-03-01 11:35:05 +0100

    metas

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {Meta, Title} from "@angular/platform-browser";
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss']
+})
+export class TestComponent implements OnInit {
+
+  constructor(    private meta: Meta,private title: Title) { }
+
+  ngOnInit(): void {
+    this.meta.addTags([
+      {name: 'description', content: 'nizar unique satwalcry'},
+      {name: 'author', content: 'buttercms'},
+      {name: 'keywords', content: 'Angular, ButterCMS'},
+      {property: 'og:url', content: 'https://sdev.satt.atayen.us'},
+      {property: 'og:site_name', content: 'Satt Welcome page'},
+      {property: 'og:title', content: 'Satt Welcome page'},
+      {property: 'og:description', content: 'Satt Welcome page'},
+      {property: 'og:image', content: 'https://satt-token.com/assets/img/index/farm_post.png'},
+      {property: 'og:image:secure_url', content: 'https://satt-token.com/assets/img/index/farm_post.png'},
+    ]);
+    this.title.setTitle( 'Home SATT Page' );
+
+  }
+
+}
