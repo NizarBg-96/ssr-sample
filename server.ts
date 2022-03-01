2c1454a6e45786318e03de71ea2d006eec452278
Nizar <nizar@atayen.us>
2022-03-01 11:25:32 +0100

    changing port

--- server.ts
+++ server.ts
@@ -38,7 +38,7 @@ export function app(): express.Express {
 }
 
 function run(): void {
-  const port = process.env.PORT || 4000;
+  const port =  5000;
 
   // Start up the Node server
   const server = app();
